10c1ef8506b946d4a84cf510d1b1040c8c4f14c1
Gurarman Dhanoa <84045281+OceanTubez@users.noreply.github.com>
2023-05-30 13:23:07 -0700

    Created CosmeticSystem.lua
    Base file for extra changes, adding functions later!

---
+++ GuiClient/CosmeticSystem.lua
@@ -0,0 +1,9 @@
+local CosmeticFrame = script.Parent
+local ItemFrame = CosmeticFrame:WaitForChild("ItemFrame")
+local DisplayFrame = CosmeticFrame:WaitForChild("DisplayFrame")
+local CollectionFrame = CosmeticFrame:WaitForChild("CollectionFrame")
+
+local ShirtButton = CollectionFrame:WaitForChild("ShirtButton")
+local MaskButton = MasksButton:WaitForChild("MaskButton")
+local PantButton = PantButton:WaitForChild("PantButton")
+
