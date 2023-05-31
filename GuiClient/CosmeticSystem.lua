23e01ead96a2acce278c35db2f2cca3410113474
Gurarman Dhanoa <84045281+OceanTubez@users.noreply.github.com>
2023-05-31 09:50:23 -0700

    Update CosmeticSystem.lua

--- GuiClient/CosmeticSystem.lua
+++ GuiClient/CosmeticSystem.lua
@@ -6,4 +6,27 @@ local CollectionFrame = CosmeticFrame:WaitForChild("CollectionFrame")
 local ShirtButton = CollectionFrame:WaitForChild("ShirtButton")
 local MaskButton = MasksButton:WaitForChild("MaskButton")
 local PantButton = PantButton:WaitForChild("PantButton")
+local EmoteButton = EmoteButton:WaitForChild("EmoteButton")
 
+local RS = game:GetService("ReplicatedStorage")
+local CosmeticEventFolder = game:GetService("CosmeticSystem")
+local GetData = game:GetService("GetData")
+
+local Data = GetData:FireServer()
+local TemplateItem = CosmeticFrame:WaitForChild("Templates"):WaitForChild("ItemTemplate")
+
+function LoadItems(type)
+  
+  for i, item in pairs(Data)
+    
+    if 
+  
+  end
+  
+end)
+
+ShirtButton.Activated:Connect(function()
+    
+    LoadItems("Shirt")
+    
+end)
