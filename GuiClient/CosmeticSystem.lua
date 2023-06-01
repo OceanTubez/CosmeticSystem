e1e9db9796efc06b6072dc7c5ebe072a0d3335ce
Gurarman Dhanoa <84045281+OceanTubez@users.noreply.github.com>
2023-05-31 22:06:46 -0700

    Update CosmeticSystem.lua

--- GuiClient/CosmeticSystem.lua
+++ GuiClient/CosmeticSystem.lua
@@ -1,104 +1,132 @@
 local CosmeticFrame = script.Parent
-local ItemFrame = CosmeticFrame:WaitForChild("ItemFrame")
-local DisplayFrame = CosmeticFrame:WaitForChild("DisplayFrame")
-local CollectionFrame = CosmeticFrame:WaitForChild("CollectionFrame")
+local ItemFrame = CosmeticFrame:WaitForChild("Items")
+-- local DisplayFrame = CosmeticFrame:WaitForChild("Display")
+local CollectionFrame = CosmeticFrame:WaitForChild("Collection")
 
 local ShirtButton = CollectionFrame:WaitForChild("ShirtButton")
-local MaskButton = CollectionFrame:WaitForChild("MaskButton")
-local PantButton = CollectionFrame:WaitForChild("PantButton")
-local EmoteButton = CollectionFrame:WaitForChild("EmoteButton")
+-- local MaskButton = CollectionFrame:WaitForChild("MaskButton")
+-- local PantButton = CollectionFrame:WaitForChild("PantButton")
+-- local EmoteButton = CollectionFrame:WaitForChild("EmoteButton")
 
 local RS = game:GetService("ReplicatedStorage")
-local CosmeticEventFolder = game:GetService("CosmeticSystem")
-local GetData = game:GetService("GetData")
+local CosmeticEventFolder = RS:WaitForChild("CosmeticSystem")
+local GetData = CosmeticEventFolder:WaitForChild("GetData")
 
-local Data = GetData:FireServer()
+local Data = nil
 local TemplateItem = CosmeticFrame:WaitForChild("Templates"):WaitForChild("ItemTemplate")
 
+GetData:FireServer()
+
+GetData.OnClientEvent:Connect(function(retrieveddata)
+	
+	Data = retrieveddata
+	
+end)
+
 local TierColorData = {
-  
-  "Common": (201, 194, 111),
-  "Uncommon": (133, 212, 212),
-  "Rare": (133, 212, 212),
-  "Legendary": (248, 252, 0),
-  "Mythic": (252, 13, 0)
-  
+
+	Common = Color3.fromRGB(201, 194, 111),
+	Uncommon = Color3.fromRGB(133, 212, 212),
+	Rare = Color3.fromRGB(133, 212, 212),
+	Legendary = Color3.fromRGB(248, 252, 0),
+	Mythic = Color3.fromRGB(252, 13, 0)
+
 }
 
-function LoadItems(type)
-  
-  for i, item in ipairs(ItemFrame:GetChildren())
-  
-    item:Destroy()
-  
-  end
-  
-  for i, item in pairs(Data)
-    
-    if string.match(item, "Common") == true then
-      
-      local CloneItem = TemplateItem:Clone()
-      CloneItem.TierBand.BackGroundColor3 = TierColorData["Common"]
-      CloneItem.Label.Text = item
-      CloneItem.Parent = ItemFrame
-  
-    elseif string.match(item, "Uncommon") == true then
-  
-      local CloneItem = TemplateItem:Clone()
-      CloneItem.TierBand.BackGroundColor3 = TierColorData["Uncommon"]
-      CloneItem.Label.Text = item
-      CloneItem.Parent = ItemFrame
-  
-    elseif string.match(item, "Rare") == true then
-  
-      local CloneItem = TemplateItem:Clone()
-      CloneItem.TierBand.BackGroundColor3 = TierColorData["Rare"]
-      CloneItem.Label.Text = item
-      CloneItem.ValueLabel.Visible = true
-      CloneItem.Parent = ItemFrame
-  
-    elseif string.match(item, "Legendary") == true then
-  
-      local CloneItem = TemplateItem:Clone()
-      CloneItem.TierBand.BackGroundColor3 = TierColorData["Legendary"]
-      CloneItem.Label.Text = item
-      CloneItem.ValueLabel.Visible = true
-      CloneItem.Parent = ItemFrame
-  
-    elseif string.match(item, "Mythic") == true then
-  
-      local CloneItem = TemplateItem:Clone()
-      CloneItem.TierBand.BackGroundColor3 = TierColorData["Mythic"]
-      CloneItem.Label.Text = item
-      CloneItem.ValueLabel.Visible = true
-      CloneItem.Parent = ItemFrame
-    
-    end
-  
-  end
-  
-end)
+function LoadItems(equipment)
+
+	for i, item in ipairs(ItemFrame:GetChildren()) do
+		
+		if item.Name ~= "UIGridLayout" then
+			
+			item:Destroy()
+			
+		end
+		
+	end
+	
+	for i, item in pairs(Data) do
+
+		if string.sub(item, 1, 6) == "Common" then
+
+			local CloneItem = TemplateItem:Clone()
+			CloneItem.TierBand.BackgroundColor3 = TierColorData["Common"]
+			CloneItem.Label.Text = item
+			CloneItem.Label.TextStrokeColor3 = TierColorData["Common"]
+			CloneItem.Visible = true
+			CloneItem.Parent = ItemFrame
+
+		elseif string.sub(item, 1, 8) == "Uncommon" then
+
+			local CloneItem = TemplateItem:Clone()
+			CloneItem.TierBand.BackgroundColor3 = TierColorData["Uncommon"]
+			CloneItem.Label.Text = item
+			CloneItem.Label.TextStrokeColor3 = TierColorData["Uncommon"]
+			CloneItem.Visible = true
+			CloneItem.Parent = ItemFrame
+
+		elseif string.sub(item, 1, 4) == "Rare" then
+
+			local CloneItem = TemplateItem:Clone()
+			CloneItem.TierBand.BackgroundColor3 = TierColorData["Rare"]
+			CloneItem.Label.Text = item
+			CloneItem.Label.TextStrokeColor3 = TierColorData["Rare"]
+			CloneItem.ValueLabel.Visible = true
+			CloneItem.Visible = true
+			CloneItem.Parent = ItemFrame
+
+		elseif string.sub(item, 1, 9) == "Legendary" then
+
+			local CloneItem = TemplateItem:Clone()
+			CloneItem.TierBand.BackgroundColor3 = TierColorData["Legendary"]
+			CloneItem.Label.Text = item
+			CloneItem.Label.TextStrokeColor3 = TierColorData["Legendary"]
+			CloneItem.ValueLabel.Visible = true
+			CloneItem.Visible = true
+			CloneItem.Parent = ItemFrame
+
+		elseif string.sub(item, 1, 6) == "Mythic" then
+
+			local CloneItem = TemplateItem:Clone()
+			CloneItem.TierBand.BackgroundColor3 = TierColorData["Mythic"]
+			CloneItem.Label.Text = item
+			CloneItem.Label.TextStrokeColor3 = TierColorData["Mythic"]
+			CloneItem.ValueLabel.Visible = true
+			CloneItem.Visible = true
+			CloneItem.Parent = ItemFrame
+
+		end
+
+	end
+	
+end
 
 ShirtButton.Activated:Connect(function()
-    
-    LoadItems("Shirt")
-    
+	
+	print("got it")
+	
+	LoadItems("Shirt")
+	
 end)
 
+--[[
+
 PantButton.Activated:Connect(function()
-    
-    LoadItems("Pant")
-    
+
+	LoadItems("Pant")
+
 end)
 
 MaskButton.Activated:Connect(function()
-    
-    LoadItems("Mask")
-    
+
+	LoadItems("Mask")
+
 end)
 
 EmoteButton.Activated:Connect(function()
-    
-    LoadItems("Emote")
-    
+
+	LoadItems("Emote")
+
 end)
+
+]]--
